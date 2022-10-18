6d3595bc8a4e4b63874a56aa87a2c5682438098e
Mutiu Abdulrasaq <83947644+AbdulrasaqM@users.noreply.github.com>
2022-10-18 16:36:14 +0100

    Update converter.c

--- converter.c
+++ converter.c
@@ -6,6 +6,7 @@
  * @base: input base
  * @lowercase: flag if hexa values need to be lowercase
  * Return: result string
+ * Authors: Ehoneah Obed & Abdulhakeem Badejo
  */
 char *convert(unsigned long int num, int base, int lowercase)
 {
@@ -14,13 +15,11 @@ char *convert(unsigned long int num, int base, int lowercase)
 	char *ptr;
 
 	rep = (lowercase)
-? "0123456789abcdef"
-: "0123456789ABCDEF";
+		? "0123456789abcdef"
+		: "0123456789ABCDEF";
 	ptr = &buffer[49];
 	*ptr = '\0';
-	do
-
-	{
+	do {
 		*--ptr = rep[num % base];
 		num /= base;
 	} while (num != 0);
