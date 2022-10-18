259798082b59d50bd94da7bb737f00c824eaee6b
prazido.bitty <111038897+bitty007@users.noreply.github.com>
2022-10-18 16:51:52 +0100

    print base

--- print_bases.c
+++ print_bases.c
@@ -12,14 +12,14 @@
  */
 int print_hex(va_list l, flags_t *f)
 {
-	unsigned int num = va_arg(l, unsigned int);
-	char *str = convert(num, 16, 1);
-	int count = 0;
+    unsigned int num = va_arg(l, unsigned int);
+    char *str = convert(num, 16, 1);
+    int count = 0;
 
-	if (f->hash == 1 && str[0] != '0')
-		count += _puts("0x");
-	count += _puts(str);
-	return (count);
+    if (f->hash == 1 && str[0] != '0')
+        count += _puts("0x");
+    count += _puts(str);
+    return (count);
 }
 
 /**
@@ -34,14 +34,14 @@ int print_hex(va_list l, flags_t *f)
  */
 int print_hex_big(va_list l, flags_t *f)
 {
-	unsigned int num = va_arg(l, unsigned int);
-	char *str = convert(num, 16, 0);
-	int count = 0;
+    unsigned int num = va_arg(l, unsigned int);
+    char *str = convert(num, 16, 0);
+    int count = 0;
 
-	if (f->hash == 1 && str[0] != '0')
-		count += _puts("0X");
-	count += _puts(str);
-	return (count);
+    if (f->hash == 1 && str[0] != '0')
+        count += _puts("0X");
+    count += _puts(str);
+    return (count);
 }
 
 /**
@@ -55,11 +55,11 @@ int print_hex_big(va_list l, flags_t *f)
  */
 int print_binary(va_list l, flags_t *f)
 {
-	unsigned int num = va_arg(l, unsigned int);
-	char *str = convert(num, 2, 0);
+    unsigned int num = va_arg(l, unsigned int);
+    char *str = convert(num, 2, 0);
 
-	(void)f;
-	return (_puts(str));
+    (void)f;
+    return (_puts(str));
 }
 
 /**
@@ -73,12 +73,12 @@ int print_binary(va_list l, flags_t *f)
  */
 int print_octal(va_list l, flags_t *f)
 {
-	unsigned int num = va_arg(l, unsigned int);
-	char *str = convert(num, 8, 0);
-	int count = 0;
+    unsigned int num = va_arg(l, unsigned int);
+    char *str = convert(num, 8, 0);
+    int count = 0;
 
-	if (f->hash == 1 && str[0] != '0')
-		count += _putchar('0');
-	count += _puts(str);
-	return (count);
+    if (f->hash == 1 && str[0] != '0')
+        count += _putchar('0');
+    count += _puts(str);
+    return (count);
 }
